9ea99b821e763ce0065aeb725c7591941c38cca4
Joshua Bohde <bohdecoded@gmail.com>
2023-08-19 09:01:08 -0400

    feat: install react-cookie and jwt-decode and implement login and registration functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,52 @@
-import './App.css';
+import "./App.css";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { Outlet } from 'react-router-dom';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Outlet } from "react-router-dom";
+import { CookiesProvider, useCookies } from "react-cookie";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { CurrentUserProvider } from "./context";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+
+
+function App() {
+const [cookies] = useCookies(['auth_token']);
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
+  const authLink = setContext((_, { headers }) =>
+  ({
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: cookies?.auth_token ? `Bearer ${cookies.auth_token}` : "",
     },
-  };
-});
+  }));
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
-function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Outlet />
-        </div>
-        <Footer />
-      </div>
+      <CookiesProvider>
+        <CurrentUserProvider>
+            <Header />
+            <main>
+              <Outlet />
+            </main>
+            <Footer />
+        </CurrentUserProvider>
+      </CookiesProvider>
     </ApolloProvider>
   );
 }
