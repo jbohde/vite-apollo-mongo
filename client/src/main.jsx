bfd8b9b64be72d4509e7c8910fc68975dfa4fc35
Joshua Bohde <bohdecoded@gmail.com>
2023-08-19 07:37:32 -0400

    feat: install react router and create skeleton routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+import App from './App';
+import Error from './pages/Error';
+import Landing from './pages/Landing';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<Error />}>
+      <Route index element={<Landing />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="dashboard" element={<Dashboard />} />
+    </Route>
+  )
+);
+
+ReactDOM
+  .createRoot(document.getElementById("root"))
+  .render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
