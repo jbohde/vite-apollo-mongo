2a04f19890a3e24416feba5c2adc7e85a18718d6
Joshua Bohde <bohdecoded@gmail.com>
2023-08-19 09:01:29 -0400

    feat: add basic styles for application

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>Footer</footer>
   )
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,24 @@
-function Header() {
-  return (
-    <div>Header</div>
-  )
-}
+import { Link } from 'react-router-dom';
+import { useCurrentUserContext } from '../context/CurrentUser';
 
-export default Header
+ function Header() {
+  const { isLoggedIn, logoutUser } = useCurrentUserContext();
+
+  return (
+    <nav>
+      {isLoggedIn() ? (
+        <>
+          <Link to="/dashboard">Dashboard</Link>
+          <button type="button" onClick={logoutUser}>Logout</button>
+        </>
+      ) : (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Sign Up</Link>
+        </>
+      )}
+    </nav>
+  );
+ }
+
+ export default Header
