dcd8425b371c6048628fe7089a363a7ec8458ff5
Joshua Bohde <bohdecoded@gmail.com>
2023-08-23 21:24:20 -0400

    fix: hash seeded user passwords

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable func-names */
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcryptjs');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema({
   firstName: {
@@ -17,7 +18,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
+    match: [/.+@.+\..+/, "Must match an email address!"],
   },
   password: {
     type: String,
@@ -26,18 +27,48 @@ const userSchema = new Schema({
   },
 });
 
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
   next();
 });
 
+userSchema.pre("insertMany", async (next, docs) => {
+  if (Array.isArray(docs) && docs.length) {
+    const hashedUsers = docs.map(
+      async (user) =>
+        new Promise((resolve, reject) => {
+          bcrypt
+            .genSalt(10)
+            .then((salt) => {
+              const password = user.password.toString();
+              return bcrypt
+                .hash(password, salt)
+                .then((hash) => {
+                  user.password = hash;
+                  resolve();
+                })
+                .catch((e) => {
+                  reject(e);
+                });
+            })
+            .catch((e) => {
+              reject(e);
+            });
+        })
+    );
+    docs = await Promise.all(hashedUsers);
+    return next();
+  }
+  return next(new Error("User list should not be empty")); // lookup early return pattern
+});
+
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
